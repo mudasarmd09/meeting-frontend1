beb8e7ef5748ac2ab73a64fae46fe78f8a800979
mudasarsafdar09 <mudasar077689@gmail.com>
2025-07-05 18:33:13 +0500

    update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://meeting-backend1.vercel.app/api", 
+  baseURL: "https://meeting-backend1-production.up.railway.app/api", 
   withCredentials: true, 
 });
 
